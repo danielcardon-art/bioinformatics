779b95bfc5c2862dea8ec1f49adffd333c9da438
danielcardon-art <cardoneiro@icloud.com>
2026-06-16 10:51:22 +0200

    Add DNA base counting function
    The function counts occurrences of each DNA base in a given sequence and returns the counts in a formatted string.

--- countindDNA.py
+++ countindDNA.py
@@ -1,1 +1,25 @@
+def counter_DNA(sequence):
+  countA=0
+  countC=0
+  countG=0
+  countT=0
+  
+  for base in sequence:
+    if base=="A":
+      countA+=1
+    elif base=="G":
+      countG+=1
+    elif base=="C":
+      countC+=1
+    else: countT+=1
+  
+  
+  return "The sequence has the following count of bases: A= " + str(countA) + ", C= " +str(countC) +  ", T= " + str(countT) +  ", G= " + str(countG)
 
+
+
+
+print(counter_DNA("AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"))
+
+    
+    
